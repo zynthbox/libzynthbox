9a07cd5782d93812fb67f751aa0fcace9550f97a
Anupam Basak <anupam.basak27@gmail.com>
2022-08-04 12:52:12 +0530

    libzl, CAS : Add new property named Pan to allow audio panning

--- lib/libzl.cpp
+++ lib/libzl.cpp
@@ -283,6 +283,10 @@ public:
     c->setLength(beat, bpm);
   }
 
+  void setClipPan(ClipAudioSource *c, float pan) {
+    c->setPan(pan);
+  }
+
   void setClipStartPosition(ClipAudioSource *c, float startPositionInSeconds) {
     c->setStartPosition(startPositionInSeconds);
   }
@@ -396,6 +400,13 @@ void ClipAudioSource_setLength(ClipAudioSource *c, float beat, int bpm) {
   Helper::callFunctionOnMessageThread(
       [&]() {
         elThread.setClipLength(c, beat, bpm);
+      }, true);
+}
+
+void ClipAudioSource_setPan(ClipAudioSource *c, float pan) {
+  Helper::callFunctionOnMessageThread(
+      [&]() {
+        elThread.setClipPan(c, pan);
       }, true);
 }
 
