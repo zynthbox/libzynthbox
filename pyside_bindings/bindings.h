9cd0d37c3cd19cdf93983701b34b5b4db71dbaef
Anupam Basak <anupam.basak27@gmail.com>
2023-04-19 18:29:41 +0530

    Generate bindings for PlayGridManager

--- pyside_bindings/bindings.h
+++ pyside_bindings/bindings.h
@@ -5,5 +5,6 @@
 #include "../src/AudioLevels.h"
 #include "../src/ClipAudioSource.h"
 #include "../src/Plugin.h"
+#include "../src/PlayGridManager.h"
 #include "../src/SyncTimer.h"
 #include "../src/MidiRouter.h"
