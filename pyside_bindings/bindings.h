2b99eb46f1746021c1ba948d95382d4f5ed2a3c2
Anupam Basak <anupam.basak27@gmail.com>
2023-04-19 14:12:23 +0530

    Generate bindings for the classes : AudioLevels, ClipAudioSource, Plugin, SyncTimer, MidiRouter

--- pyside_bindings/bindings.h
+++ pyside_bindings/bindings.h
@@ -3,4 +3,7 @@
 #define JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED 1
 
 #include "../src/AudioLevels.h"
+#include "../src/ClipAudioSource.h"
 #include "../src/Plugin.h"
+#include "../src/SyncTimer.h"
+#include "../src/MidiRouter.h"
