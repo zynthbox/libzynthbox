daf954077c70f53637f21630fb25813822e5893e
Anupam Basak <anupam.basak27@gmail.com>
2023-06-09 14:28:51 +0530

    Temporarily disable core-isolation

--- src/JackThreadAffinitySetter.cpp
+++ src/JackThreadAffinitySetter.cpp
@@ -19,23 +19,23 @@ void zl_set_jack_client_affinity(jack_client_t *client) {
 
 void zl_set_dsp_thread_affinity(const pthread_t& threadID)
 {
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    // Our CPU set for the DSP is 0 (where the kernel also lives), 1, and 2. This leaves 4 for the UI application
-    CPU_SET(0, &cpuset);
-    CPU_SET(1, &cpuset);
-    CPU_SET(2, &cpuset);
-    int result = pthread_setaffinity_np(threadID, sizeof(cpuset), &cpuset);
-    if (result != 0) {
-        errno = result;
-        perror("pthread_getaffinity_np");
-#if DEBUG_JACK_THREAD_AFFINITY_SETTER
-    } else {
-        QStringList accepted;
-        for (size_t j = 0; j < CPU_SETSIZE; j++)
-            if (CPU_ISSET(j, &cpuset))
-                accepted << QString("CPU %1").arg(QString::number(j));
-        qDebug() << "Set returned by pthread_getaffinity_np() contained:" << accepted.join(", ");
-#endif
-    }
+//     cpu_set_t cpuset;
+//     CPU_ZERO(&cpuset);
+//     // Our CPU set for the DSP is 0 (where the kernel also lives), 1, and 2. This leaves 4 for the UI application
+//     CPU_SET(0, &cpuset);
+//     CPU_SET(1, &cpuset);
+//     CPU_SET(2, &cpuset);
+//     int result = pthread_setaffinity_np(threadID, sizeof(cpuset), &cpuset);
+//     if (result != 0) {
+//         errno = result;
+//         perror("pthread_getaffinity_np");
+// #if DEBUG_JACK_THREAD_AFFINITY_SETTER
+//     } else {
+//         QStringList accepted;
+//         for (size_t j = 0; j < CPU_SETSIZE; j++)
+//             if (CPU_ISSET(j, &cpuset))
+//                 accepted << QString("CPU %1").arg(QString::number(j));
+//         qDebug() << "Set returned by pthread_getaffinity_np() contained:" << accepted.join(", ");
+// #endif
+//     }
 }
