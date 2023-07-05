e646e4b94f91c1fa30d0d37a951a9d783a6b0139
Anupam Basak <anupam.basak27@gmail.com>
2023-07-05 15:47:24 +0530

    Add install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Stop all running heavy processes
+systemctl stop jack2 zynthbox-qml zynthian-webconf
+
+(
+# Create build dir and cd into it
+mkdir -p $SCRIPTPATH/build && cd $SCRIPTPATH/build
+
+# Configure
+cmake -DCMAKE_INSTALL_SYSCONFDIR=/etc -DCMAKE_INSTALL_LOCALSTATEDIR=/var -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON -DCMAKE_FIND_USE_PACKAGE_REGISTRY=OFF -DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON -DFETCHCONTENT_FULLY_DISCONNECTED=ON -DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_INSTALL_LIBDIR=lib/arm-linux-gnueabihf -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+
+# Build
+make -j$(nproc) "INSTALL=install --strip-program=true" VERBOSE=1
+
+# Install
+make -j$(nproc) install AM_UPDATE_INFO_DIR=no "INSTALL=install --strip-program=true"
+)
+
+# Start the stopped processes
+systemctl start jack2 zynthbox-qml zynthian-webconf
