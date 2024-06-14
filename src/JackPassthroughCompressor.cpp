7ae668e0ba738c9e5d3705e0067f29698120a253
Dan Leinir Turthra Jensen <admin@leinir.dk>
2024-06-14 13:38:38 +0100

    Add settings container for the JackPassthrough compressor

---
+++ src/JackPassthroughCompressor.cpp
@@ -0,0 +1,200 @@
+/**
+ *    JackPassthroughCompressor.h
+ *    Created: 13 June, 2024
+ *    Author: Dan Leinir Turthra Jensen <admin@leinir.dk>
+ */
+
+#include "JackPassthroughCompressor.h"
+
+class JackPassthroughCompressorPrivate {
+public:
+    JackPassthroughCompressorPrivate() {}
+
+    QString name;
+    bool selected{false};
+    float sampleRate{48000};
+    bool parametersChanged{true};
+    float threshold{-10.0f}; // Threshold (dB)
+    juce::NormalisableRange<float> thresholdRange{-50.0f, 10.0f, 0.1f};
+    float kneeWidth{0.0f}; // Knee Width (dB)
+    juce::NormalisableRange<float> kneeWidthRange{0.0f, 30.0f, 0.1f};
+    float attack{30.f}; // Attack Time (ms)
+    juce::NormalisableRange<float> attackRange{0.0f, 100.0f, 0.1f};
+    float release{150.0f}; // Release Time (ms)
+    juce::NormalisableRange<float> releaseRange{0.0f, 500.0f, 0.1f};
+    float ratio{4.0f}; // Ratio ( : 1)
+    juce::NormalisableRange<float> ratioRange{1.0f, 16.0f, 0.1f};
+    float makeUpGain{0.0f}; // Make Up Gain (dB)
+    juce::NormalisableRange<float> makeUpGainRange{-10.0f, 20.0f, 0.1f};
+};
+
+JackPassthroughCompressor::JackPassthroughCompressor(JackPassthrough* parent)
+    : QObject(parent)
+    , d(new JackPassthroughCompressorPrivate)
+{
+}
+
+JackPassthroughCompressor::~JackPassthroughCompressor()
+{
+    delete d;
+}
+
+QString JackPassthroughCompressor::name() const
+{
+    return d->name;
+}
+
+void JackPassthroughCompressor::setName(const QString& name)
+{
+    if (d->name != name) {
+        d->name = name;
+        Q_EMIT nameChanged();
+    }
+}
+
+bool JackPassthroughCompressor::selected() const
+{
+    return d->selected;
+}
+
+void JackPassthroughCompressor::setSelected(const bool& selected)
+{
+    if (d->selected != selected) {
+        d->selected = true;
+        Q_EMIT selectedChanged();
+    }
+}
+
+float JackPassthroughCompressor::threshold() const
+{
+    return d->thresholdRange.convertTo0to1(d->threshold);
+}
+
+void JackPassthroughCompressor::setThreshold(const float& threshold)
+{
+    setThresholdDB(d->thresholdRange.convertFrom0to1(threshold));
+}
+
+float JackPassthroughCompressor::thresholdDB() const
+{
+    return d->threshold;
+}
+
+void JackPassthroughCompressor::setThresholdDB(const float& thresholdDB)
+{
+    if (d->threshold != thresholdDB) {
+        d->threshold = d->thresholdRange.getRange().clipValue(thresholdDB);
+        Q_EMIT thresholdChanged();
+    }
+}
+
+float JackPassthroughCompressor::kneeWidth() const
+{
+    return d->kneeWidthRange.convertTo0to1(d->kneeWidth);
+}
+
+void JackPassthroughCompressor::setKneeWidth(const float& kneeWidth)
+{
+    setKneeWidthDB(d->kneeWidthRange.convertFrom0to1(kneeWidth));
+}
+
+float JackPassthroughCompressor::kneeWidthDB() const
+{
+    return d->kneeWidth;
+}
+
+void JackPassthroughCompressor::setKneeWidthDB(const float& kneeWidthDB)
+{
+    if (d->kneeWidth != kneeWidthDB) {
+        d->kneeWidth = d->kneeWidthRange.getRange().clipValue(kneeWidthDB);
+        Q_EMIT kneeWidthChanged();
+    }
+}
+
+float JackPassthroughCompressor::attack() const
+{
+    return d->attack;
+}
+
+void JackPassthroughCompressor::setAttack(const float& attack)
+{
+    if (d->attack != attack) {
+        d->attack = d->attackRange.getRange().clipValue(attack);
+        Q_EMIT attackChanged();
+    }
+}
+
+float JackPassthroughCompressor::release() const
+{
+    return d->release;
+}
+
+void JackPassthroughCompressor::setRelease(const float& release)
+{
+    if (d->release != release) {
+        d->release = d->releaseRange.getRange().clipValue(release);
+        Q_EMIT releaseChanged();
+    }
+}
+
+float JackPassthroughCompressor::ratio() const
+{
+    return d->ratio;
+}
+
+void JackPassthroughCompressor::setRatio(const float& ratio)
+{
+    if (d->ratio != ratio) {
+        d->ratio = d->ratioRange.getRange().clipValue(ratio);
+        Q_EMIT ratioChanged();
+    }
+}
+
+float JackPassthroughCompressor::makeUpGain() const
+{
+    return d->makeUpGainRange.convertTo0to1(d->makeUpGain);
+}
+
+void JackPassthroughCompressor::setMakeUpGain(const float& makeUpGain)
+{
+    setMakeUpGainDB(d->makeUpGainRange.convertFrom0to1(makeUpGain));
+}
+
+float JackPassthroughCompressor::makeUpGainDB() const
+{
+    return d->makeUpGain;
+}
+
+void JackPassthroughCompressor::setMakeUpGainDB(const float makeUpGainDB)
+{
+    if (d->makeUpGain != makeUpGainDB) {
+        d->makeUpGain = d->makeUpGainRange.getRange().clipValue(makeUpGainDB);
+        Q_EMIT makeUpGainChanged();
+    }
+}
+
+void JackPassthroughCompressor::updateParameters()
+{
+    if (d->parametersChanged) {
+        d->parametersChanged = false;
+        for (int channelIndex = 0; channelIndex < 2; ++channelIndex) {
+            compressors[channelIndex].setThreshold(d->threshold);
+            compressors[channelIndex].setKnee(d->kneeWidth);
+            compressors[channelIndex].setAttackTime(d->attack * 0.001f);
+            compressors[channelIndex].setReleaseTime(d->release * 0.001f);
+            compressors[channelIndex].setRatio(d->ratio > 15.9f ? INFINITY : d->ratio);
+            compressors[channelIndex].setMakeUpGain(d->makeUpGain);
+        }
+    }
+}
+
+void JackPassthroughCompressor::setSampleRate(const float& sampleRate)
+{
+    juce::dsp::ProcessSpec spec;
+    spec.sampleRate = sampleRate;
+    spec.numChannels = 1;
+    spec.maximumBlockSize = 8192;
+    for (int channelIndex = 0; channelIndex < 2; ++channelIndex) {
+        compressors[channelIndex].prepare(spec);
+    }
+}
---
+++ src/JackPassthroughCompressor.h
@@ -0,0 +1,70 @@
+/**
+ *    JackPassthroughCompressor.h
+ *    Created: 13 June, 2024
+ *    Author: Dan Leinir Turthra Jensen <admin@leinir.dk>
+ */
+
+#pragma once
+
+#include "JackPassthrough.h"
+#include "Compressor.h"
+
+class JackPassthroughCompressorPrivate;
+class JackPassthroughCompressor : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name NOTIFY nameChanged)
+    Q_PROPERTY(bool selected READ selected WRITE setSelected NOTIFY selectedChanged)
+    Q_PROPERTY(float threshold READ threshold WRITE setThreshold NOTIFY thresholdChanged)
+    Q_PROPERTY(float thresholdDB READ thresholdDB WRITE setThresholdDB NOTIFY thresholdChanged)
+    Q_PROPERTY(float kneeWidth READ kneeWidth WRITE setKneeWidth NOTIFY kneeWidthChanged)
+    Q_PROPERTY(float kneeWidthDB READ kneeWidthDB WRITE setKneeWidthDB NOTIFY kneeWidthChanged)
+    Q_PROPERTY(float attack READ attack WRITE setAttack NOTIFY attackChanged)
+    Q_PROPERTY(float release READ release WRITE setRelease NOTIFY releaseChanged)
+    Q_PROPERTY(float ratio READ ratio WRITE setRatio NOTIFY ratioChanged)
+    Q_PROPERTY(float makeUpGain READ makeUpGain WRITE setMakeUpGain NOTIFY makeUpGainChanged)
+    Q_PROPERTY(float makeUpGainDB READ makeUpGainDB WRITE setMakeUpGainDB NOTIFY makeUpGainChanged)
+public:
+    explicit JackPassthroughCompressor(JackPassthrough *parent = nullptr);
+    ~JackPassthroughCompressor() override;
+
+    QString name() const;
+    void setName(const QString &name);
+    Q_SIGNAL void nameChanged();
+    bool selected() const;
+    void setSelected(const bool &selected);
+    Q_SIGNAL void selectedChanged();
+
+    float threshold() const;
+    void setThreshold(const float &threshold);
+    float thresholdDB() const;
+    void setThresholdDB(const float &thresholdDB);
+    Q_SIGNAL void thresholdChanged();
+    float kneeWidth() const;
+    void setKneeWidth(const float &kneeWidth);
+    float kneeWidthDB() const;
+    void setKneeWidthDB(const float &kneeWidthDB);
+    Q_SIGNAL void kneeWidthChanged();
+    float attack() const;
+    void setAttack(const float &attack);
+    Q_SIGNAL void attackChanged();
+    float release() const;
+    void setRelease(const float &release);
+    Q_SIGNAL void releaseChanged();
+    float ratio() const;
+    void setRatio(const float &ratio);
+    Q_SIGNAL void ratioChanged();
+    float makeUpGain() const;
+    void setMakeUpGain(const float &makeUpGain);
+    float makeUpGainDB() const;
+    void setMakeUpGainDB(const float makeUpGainDB);
+    Q_SIGNAL void makeUpGainChanged();
+
+    void setSampleRate(const float &sampleRate);
+
+    // Called at the start of each process call to update the filters internal state, so needs to be very low impact
+    void updateParameters();
+
+    iem::Compressor compressors[2];
+private:
+    JackPassthroughCompressorPrivate *d{nullptr};
+};
