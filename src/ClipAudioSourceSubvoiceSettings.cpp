4096a7d688149b3844ca77a28fdff9648e11b782
Dan Leinir Turthra Jensen <admin@leinir.dk>
2024-07-18 11:09:08 +0100

    CASSubvoiceSettings: New mini-container for subvoice settings
    This is a simple data container which just manages the settings for an
    individual subvoice as pertaining to a ClipAudioSource. The sub-voices
    are technically full voices, but they are launched alongside the prime
    voice (and stopped accordingly), which have adjustments made for pan,
    pitch, and gain. This allows us to create more proper chorus and unison
    style things, without having to occupy multiple sample slots on a track
    with the same sample and make an all-matching keyzone setup, leaving
    that to allow for the actual voicing logic that's supposed to allow for.

---
+++ src/ClipAudioSourceSubvoiceSettings.cpp
@@ -0,0 +1,80 @@
+#include "ClipAudioSourceSubvoiceSettings.h"
+
+#include "JUCEHeaders.h"
+
+static constexpr float maxGainDB{24.0f};
+
+class ClipAudioSourceSubvoiceSettingsPrivate{
+public:
+    ClipAudioSourceSubvoiceSettingsPrivate() {}
+    float pitch{0.0f};
+    float pitchChangePrecalc{1.0f};
+    float pan{0.0f};
+    float gain{1.0f};
+};
+
+ClipAudioSourceSubvoiceSettings::ClipAudioSourceSubvoiceSettings(QObject* parent)
+    : QObject(parent)
+    , d(new ClipAudioSourceSubvoiceSettingsPrivate)
+{
+}
+
+float ClipAudioSourceSubvoiceSettings::pan() const
+{
+    return d->pan;
+}
+
+void ClipAudioSourceSubvoiceSettings::setPan(const float& pan)
+{
+    if (d->pan != pan) {
+        d->pan = pan;
+        Q_EMIT panChanged();
+    }
+}
+
+float ClipAudioSourceSubvoiceSettings::pitch() const
+{
+    return d->pitch;
+}
+
+float ClipAudioSourceSubvoiceSettings::pitchChangePrecalc() const
+{
+    return d->pitchChangePrecalc;
+}
+
+void ClipAudioSourceSubvoiceSettings::setPitch(const float& pitch)
+{
+    if (d->pitch != pitch) {
+        d->pitch = pitch;
+        d->pitchChangePrecalc = std::pow(2.0, d->pitch / 12.0) /* * sampleRate() / sampleRate() */; // should this perhaps be a sound sample rate over playback sample rate thing?
+        Q_EMIT pitchChanged();
+    }
+}
+
+float ClipAudioSourceSubvoiceSettings::gain() const
+{
+    return d->gain;
+}
+
+float ClipAudioSourceSubvoiceSettings::gainDb() const
+{
+    return juce::Decibels::gainToDecibels(d->gain);
+}
+
+float ClipAudioSourceSubvoiceSettings::gainAbsolute() const
+{
+    return juce::jmap(juce::Decibels::gainToDecibels(d->gain, -maxGainDB), -maxGainDB, maxGainDB, 0.0f, 1.0f);
+}
+
+void ClipAudioSourceSubvoiceSettings::setGain(const float& gain)
+{
+    if (d->gain != gain && 0.0f <= gain && gain <= 15.84893192461113) {
+        d->gain = gain;
+        Q_EMIT gainChanged();
+    }
+}
+
+void ClipAudioSourceSubvoiceSettings::setGainAbsolute(const float& gainAbsolute)
+{
+    setGain(juce::Decibels::decibelsToGain(juce::jmap(gainAbsolute, 0.0f, 1.0f, -maxGainDB, maxGainDB), -maxGainDB));
+}
---
+++ src/ClipAudioSourceSubvoiceSettings.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <QObject>
+
+class ClipAudioSourceSubvoiceSettingsPrivate;
+class ClipAudioSourceSubvoiceSettings : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(float pan READ pan WRITE setPan NOTIFY panChanged)
+    Q_PROPERTY(float pitch READ pitch WRITE setPitch NOTIFY pitchChanged)
+    Q_PROPERTY(float gain READ gain WRITE setGain NOTIFY gainChanged)
+    Q_PROPERTY(float gainAbsolute READ gainAbsolute WRITE setGainAbsolute NOTIFY gainChanged)
+    Q_PROPERTY(float gainDb READ gainDb NOTIFY gainChanged)
+public:
+    explicit ClipAudioSourceSubvoiceSettings(QObject *parent = nullptr);
+    ~ClipAudioSourceSubvoiceSettings() override = default;
+
+    float pan() const;
+    void setPan(const float &pan);
+    Q_SIGNAL void panChanged();
+
+    float pitch() const;
+    float pitchChangePrecalc() const;
+    void setPitch(const float &pitch);
+    Q_SIGNAL void pitchChanged();
+
+    float gain() const;
+    float gainDb() const;
+    float gainAbsolute() const;
+    void setGain(const float &gain);
+    void setGainAbsolute(const float &gainAbsolute);
+    Q_SIGNAL void gainChanged();
+private:
+    ClipAudioSourceSubvoiceSettingsPrivate *d{nullptr};
+};
