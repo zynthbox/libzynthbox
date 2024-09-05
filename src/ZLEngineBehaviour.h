10d821824cc380de67399f4e2778284c27c78f89
Anupam Basak <anupam.basak27@gmail.com>
2024-09-05 10:32:32 +0530

    ZLEngineBehaviour : Override `addSystemAudioIODeviceTypes` to always return false

--- src/ZLEngineBehaviour.h
+++ src/ZLEngineBehaviour.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "JUCEHeaders.h"
+#include <QDebug>
 
 class ZLEngineBehavior : public tracktion::engine::EngineBehaviour {
-  bool autoInitialiseDeviceManager() override { return false; }
+    bool autoInitialiseDeviceManager() override { qDebug() << "autoInitialiseDeviceManager? : Absolutely NOT !!!"; return false; }
+    bool addSystemAudioIODeviceTypes() override { qDebug() << "addSystemAudioIODeviceTypes? : Absolutely NOT !!!"; return false; }
 };
