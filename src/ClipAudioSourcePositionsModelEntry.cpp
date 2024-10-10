a13a7c3e5fefa8c9362be6debd484e414fb7a262
Dan Leinir Turthra Jensen <admin@leinir.dk>
2024-10-10 11:17:16 +0100

    CAS Position model entry now fades gain values
    This ensures that we end up with dots which look more like what they
    would be expected to look like, rather than flickering with lots of
    changes.

--- src/ClipAudioSourcePositionsModelEntry.cpp
+++ src/ClipAudioSourcePositionsModelEntry.cpp
@@ -7,17 +7,20 @@ ClipAudioSourcePositionsModelEntry::ClipAudioSourcePositionsModelEntry(QObject* 
 
 void ClipAudioSourcePositionsModelEntry::clear()
 {
-    updateData(-1, 0, 0, 0, 0);
+    updateData(-1, -1, 0, 0, 0, 0);
+    m_gain = m_gainRight = m_gainLeft = 0;
     m_keepUntil = -1;
 }
 
-void ClipAudioSourcePositionsModelEntry::updateData(const int &id, const float& progress, const float& gainLeft, const float& gainRight, const float& pan)
+void ClipAudioSourcePositionsModelEntry::updateData(const int &id, const int& playheadId, const float& progress, const float& gainLeft, const float& gainRight, const float& pan)
 {
     m_id = id;
+    m_playheadId = playheadId;
     m_progress = progress;
-    m_gain = qMax(gainLeft, gainRight);
-    m_gainLeft = gainLeft;
-    m_gainRight = gainRight;
+    // This is an imperfect thing (the fade doesn't take time into account, just number of updates), so fades will not be amazing, but they'll happen
+    m_gainLeft = (gainLeft >= m_gainLeft) ? gainLeft : qMin(m_gainLeft * 0.9f, m_gainLeft - 0.01f);
+    m_gainRight = (gainRight >= m_gainRight) ? gainRight : qMin(m_gainRight * 0.9f, m_gainRight - 0.01f);
+    m_gain = qMax(m_gainLeft, m_gainRight);
     m_pan = pan;
     Q_EMIT dataChanged();
 }
