591c83b87150e38449a63dd5d25ddc50ede06861
Anupam Basak <anupam.basak27@gmail.com>
2025-02-25 13:25:39 +0530

    SndHelper : Implement extracting snd metadata when generating index

--- src/SndHelper.cpp
+++ src/SndHelper.cpp
@@ -4,6 +4,12 @@
 #include <QDebug>
 #include <QFileInfo>
 #include <QFileInfoList>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonValue>
+#include <QStringList>
+#include <QList>
 
 /**
  * When DEBUG is set to true it will print a set of logs
@@ -30,7 +36,33 @@ void SndHelper::serializeTo(const QString sourceDir, const QString outputFile)
         for (const QFileInfo &file: fileList) {
             IFDEBUG(qDebug() << QString("Extracting metadata from file #%1: %2").arg(++i).arg(file.fileName()));
             const auto metadata = AudioTagHelper::instance()->readWavMetadata(file.filePath());
-            // IFDEBUG(qDebug() << "  Category : " << metadata["ZYNTHBOX_SOUND_CATEGORY"]);
+            if (metadata.contains("ZYNTHBOX_SOUND_SYNTH_FX_SNAPSHOT") &&
+                metadata.contains("ZYNTHBOX_SOUND_SAMPLE_SNAPSHOT") &&
+                metadata.contains("ZYNTHBOX_SOUND_CATEGORY")
+            ) {
+                QStringList synthSlotsData = {"", "", "", "", ""};
+                QStringList sampleSlotsData = {"", "", "", "", ""};
+                QStringList fxSlotsData = {"", "", "", "", ""};
+                const QString category = metadata["ZYNTHBOX_SOUND_CATEGORY"];
+                const auto synthFxSnapshotJsonObj = QJsonDocument::fromJson(metadata["ZYNTHBOX_SOUND_SYNTH_FX_SNAPSHOT"].toUtf8()).object();
+                const auto sampleSnapshotJsonObj = QJsonDocument::fromJson(metadata["ZYNTHBOX_SOUND_SAMPLE_SNAPSHOT"].toUtf8()).object();
+                for (auto layerData : synthFxSnapshotJsonObj["layers"].toArray()) {
+                    const auto layerDataObj = layerData.toObject();
+                    const QString engineType = layerDataObj["engine_type"].toString();
+                    if (engineType == "MIDI Synth") {
+                        synthSlotsData.replace(layerDataObj["slot_index"].toInt(), QString("%1 > %2").arg(layerDataObj["engine_name"].toString().split("/").last()).arg(layerDataObj["preset_name"].toString()));
+                    } else if (engineType == "Audio Effect") {
+                        fxSlotsData.replace(layerDataObj["slot_index"].toInt(), QString("%1 > %2").arg(layerDataObj["engine_name"].toString().split("/").last()).arg(layerDataObj["preset_name"].toString()));
+                    }
+                }
+                for (int i = 0; i < sampleSnapshotJsonObj.keys().length(); ++i) {
+                    sampleSlotsData.replace(i, sampleSnapshotJsonObj[QString("%1").arg(i)].toObject()["filename"].toString());
+                }
+                IFDEBUG(qDebug() << QString("  Category : %1").arg(category));
+                IFDEBUG(qDebug() << QString("  Synth    : %1").arg(synthSlotsData.join(", ")));
+                IFDEBUG(qDebug() << QString("  Sample   : %1").arg(sampleSlotsData.join(", ")));
+                IFDEBUG(qDebug() << QString("  Fx       : %1").arg(fxSlotsData.join(", ")));
+            }
         }
     }
     IFDEBUG(qDebug() << "End Serialize");
