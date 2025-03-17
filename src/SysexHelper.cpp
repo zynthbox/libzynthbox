95940d040e12b6bdc752b60f61d28c9d11e6ffb2
Dan Leinir Turthra Jensen <admin@leinir.dk>
2025-03-17 14:50:36 +0000

    SysexHelper: Slightly better sysex id response detection

--- src/SysexHelper.cpp
+++ src/SysexHelper.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2025 Dan Leinir Turthra Jensen <admin@leinir.dk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3, or any
+ * later version accepted by the membership of KDE e.V. (or its
+ * successor approved by the membership of KDE e.V.), which shall
+ * act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "SysexHelper.h"
 #include "MidiRing.h"
 
@@ -296,7 +317,7 @@ void SysexHelper::handlePostponedEvents()
                     } else if (message.numBytes == 5 && subID == 0x06 && subID2 == 0x01) {
                         // Universal SysEx: Identity Request
                         identifiedAsSysex = true;
-                    } else if (message.numBytes == 16 && subID == 0x06 && subID2 == 0x02) {
+                    } else if (((message.numBytes == 15 && message.data[5] != 0x00)|| (message.numBytes == 17 && message.data[5] == 0x00)) && subID == 0x06 && subID2 == 0x02) {
                         // Universal SysEx: Identity Response
                         identifiedAsSysex = true;
                         isIdentityResponse = true;
