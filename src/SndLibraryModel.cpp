2b20b79f69613ffd59cd12aa766edd7ae5fe3934
Anupam Basak <anupam.basak27@gmail.com>
2025-03-26 15:10:23 +0530

    SndLibraryModel : Check if symlink target exists before adding to model

--- src/SndLibraryModel.cpp
+++ src/SndLibraryModel.cpp
@@ -71,30 +71,33 @@ void SndLibraryModel::refresh()
     m_sounds.clear();
     endRemoveRows();
 
-
+    QDir baseSoundsDir("/zynthian/zynthian-my-data/sounds/");
     QDirIterator it(m_sndLibrary->sndIndexPath(), QDir::Files, QDirIterator::Subdirectories);
     while (it.hasNext()) {
         const QFileInfo fileInfo = QFileInfo(it.next());
         if (fileInfo.isSymbolicLink() && !fileInfo.symLinkTarget().isEmpty()) {
             const QFileInfo sndFileInfo = QFileInfo(fileInfo.symLinkTarget());
-            const QString fileIdentifier = QByteArray::fromBase64(fileInfo.baseName().toUtf8(), QByteArray::Base64Encoding | QByteArray::OmitTrailingEquals);
+            const QString fileIdentifier = baseSoundsDir.relativeFilePath(sndFileInfo.filePath());
             const QString sndFileName = sndFileInfo.baseName();
             const QString origin = fileIdentifier.split("/")[0];
             const QString category = fileInfo.dir().dirName();
-            beginInsertRows(QModelIndex().parent(), m_sounds.size(), m_sounds.size());
-            if(DEBUG) qDebug() << "Reading sound index :" << fileIdentifier;
-            m_sounds.append(new SndFileInfo(
-                fileIdentifier,
-                sndFileName,
-                origin,
-                category,
-                this
-            ));
-            if (!categoriesFileCount.contains(origin + "/" + category)) {
-                categoriesFileCount[origin + "/" + category] = 0;
+            // And snd file to model only if symlink target snd file exists
+            if (sndFileInfo.exists()) {
+                beginInsertRows(QModelIndex().parent(), m_sounds.size(), m_sounds.size());
+                if(DEBUG) qDebug() << "Reading sound index :" << fileIdentifier;
+                m_sounds.append(new SndFileInfo(
+                    fileIdentifier,
+                    sndFileName,
+                    origin,
+                    category,
+                    this
+                ));
+                if (!categoriesFileCount.contains(origin + "/" + category)) {
+                    categoriesFileCount[origin + "/" + category] = 0;
+                }
+                ++categoriesFileCount[origin + "/" + category];
+                endInsertRows();
             }
-            ++categoriesFileCount[origin + "/" + category];
-            endInsertRows();
         }
         for (auto it = categoriesFileCount.constBegin(); it != categoriesFileCount.constEnd(); ++it) {
             Q_EMIT categoryFilesCountChanged(it.key().split("/")[1], it.key().split("/")[0], it.value());
