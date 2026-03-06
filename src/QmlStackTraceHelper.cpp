fb264911a75f71f7b08500af7a9fe47ea99a6afe
Dan Leinir Turthra Jensen <admin@leinir.dk>
2026-03-06 10:08:58 +0000

    QML StackTraceHelper: Optional tool for outputting a QML stack trace
    Helpful debugging assistance tool created by KDAB, useful for pulling
    information out in gdb. Set the cmake option to on, and make sure the
    required packages are installed (as it needs some private headers), and
    then follow the instructions in the cpp file to do things.

---
+++ src/QmlStackTraceHelper.cpp
@@ -0,0 +1,72 @@
+/****************************************************************************
+** Copyright (C) 2019 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com.
+** Author: sergio.martins@kdab.com
+** All rights reserved.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU Lesser General Public License version 3 as published by the
+** Free Software Foundation and appearing in the file LICENSE.LGPL.txt included.
+**
+** You may even contact us at info@kdab.com for different licensing options.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** Contact info@kdab.com if any conditions of this licensing are not
+** clear to you.
+**
+**********************************************************************/
+
+/**
+An helper function so you can get a QML backtrace from gdb.
+
+```
+(gdb) call KDAB::printQmlStackTraces()
+Stack trace for QQuickView(0x7fffffffd2e0 active exposed, visibility=QWindow::Windowed, flags=QFlags<Qt::WindowType>(Window), geometry=0,1290 500x500)
+    onSomeIndirection2Changed [qrc:/main.qml:28]
+    onSomeIndirectionChanged [qrc:/main.qml:22]
+    onClicked [qrc:/main.qml:17]
+```
+Also useful when running under `valgrind --vgdb=yes`.
+Just link `QmlStackTraceHelper.cpp` with your application.
+*/
+
+#include <QString>
+#include <QVector>
+#include <QGuiApplication>
+#include <QQuickWindow>
+#include <QQmlContext>
+#include <QQuickItem>
+#include <QtQml/private/qqmlengine_p.h>
+
+namespace KDAB {
+
+QString qmlStackTrace(QV4::ExecutionEngine *engine)
+{
+    QString result;
+    QVector<QV4::StackFrame> frames = engine->stackTrace(15);
+    for (auto &f : frames)
+        result += QStringLiteral("    %1 [%2:%3]\n").arg(f.function, f.source).arg(f.line);
+
+    return result;
+}
+
+void printQmlStackTraces()
+{
+    auto windows = qApp->topLevelWindows();
+    for (QWindow *w : windows) {
+        if (auto qw = qobject_cast<QQuickWindow*>(w)) {
+            QQuickItem *item = qw->contentItem();
+            QQmlContext* context = QQmlEngine::contextForObject(item);
+            if (!context)
+                continue;
+            QQmlEnginePrivate *enginePriv = QQmlEnginePrivate::get(context->engine());
+            QV4::ExecutionEngine *v4engine = enginePriv->v4engine();
+            qDebug() << "Stack trace for" << qw;
+            qDebug().noquote() << qmlStackTrace(v4engine);
+            qDebug() << "\n";
+        }
+    }
+}
+
+}
